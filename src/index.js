84559aa4f0d16a2c9e09e4b62ffaf73b362055d3
Carl.Frenke <carl.frenke@ladmins-MBP.fritz.box>
2020-06-24 12:12:31 +0200

    not ready

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
+const Content = ({part1, part2, part3, exercises1, exercises2, exercises3 }) => {
+  return (
+    <div>
+      <Part {part1} {exercises1}/>
+      <Part {part2} {exercises2}/>
+      <Part {part3} {exercises3}/>
+    </div>
+  )
+} 
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -14,13 +25,11 @@ const App = () => {
     <div>
       <h1>{course}</h1>
       <p>
+        <Content
         {part1} {exercises1}
-      </p>
-      <p>
         {part2} {exercises2}
-      </p>
-      <p>
         {part3} {exercises3}
+        />
       </p>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
