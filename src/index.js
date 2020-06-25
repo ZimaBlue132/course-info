af673983438a7bd4f582c5662c09cb0039a29861
Carl.Frenke <carl.frenke@ladmins-MacBook-Pro.local>
2020-06-25 13:06:49 +0200

    Not ready

--- src/index.js
+++ src/index.js
@@ -6,33 +6,29 @@ const Header = (props) => {
   return <h1>{props.course}</h1>
 }
 
-const Content = ... {
+const Content = (part1) => {
   return (
     <div>
-      <Part .../>
-      <Part .../>
-      <Part .../>
+      <Part part1/>
+      <Part parts/>
+      <Part parts/>
     </div>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
   }
 
   return (
