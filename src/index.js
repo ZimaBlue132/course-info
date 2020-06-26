64955f9c946ba13751da31795f8735d69d56ad00
Carl.Frenke <carl.frenke@ladmins-MacBook-Pro.local>
2020-06-26 12:33:20 +0200

    console.log wurde gelöscht

--- src/index.js
+++ src/index.js
@@ -14,8 +14,6 @@ const Part = ({part, exercise}) =>
 
 const Content = ({part1, part2, part3, exercises1, exercises2, exercises3 }) => {
 
-  console.log({part1, part2, part3, exercises1, exercises2, exercises3 })
-
   return (
     <div>
       <Part part={part1} exercise={exercises1}/>
